e80111581c9a172eb14f824fb0d77b3051f782ae
BicEv <bickbovev@gmail.com>
2025-05-20 18:59:54 +0300

    Separeated kafka config classes

---
+++ src/main/java/ru/bicev/notification_service/config/KafkaTopic.java
@@ -0,0 +1,16 @@
+package ru.bicev.notification_service.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopic {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("notifications").partitions(1).build();
+    }
+
+}
