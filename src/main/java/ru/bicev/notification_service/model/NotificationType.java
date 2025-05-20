6307ae6b2a3237b13f2e16857d016700a71558fe
BicEv <bickbovev@gmail.com>
2025-05-20 19:00:54 +0300

    added @JsonFormat

--- src/main/java/ru/bicev/notification_service/model/NotificationType.java
+++ src/main/java/ru/bicev/notification_service/model/NotificationType.java
@@ -1,5 +1,8 @@
 package ru.bicev.notification_service.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum NotificationType {
     EMAIL,
     PUSH,
