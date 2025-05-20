0589303944d67b7f3290f98b6712a7f9baa29ec6
BicEv <bickbovev@gmail.com>
2025-05-20 19:01:13 +0300

    Added @PostMapping

--- src/main/java/ru/bicev/notification_service/controller/NotificationController.java
+++ src/main/java/ru/bicev/notification_service/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package ru.bicev.notification_service.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class NotificationController {
         this.notificationProducer = notificationProducer;
     }
 
+    @PostMapping("/send")
     public ResponseEntity<Void> sendNotification(@RequestBody Notification notification) {
         notificationProducer.sendNotification(notification);
         return ResponseEntity.ok().build();
