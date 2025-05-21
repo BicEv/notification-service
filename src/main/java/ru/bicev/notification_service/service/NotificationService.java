acee4f3577bb2c72da0cc2771aa84900e40784c0
BicEv <bickbovev@gmail.com>
2025-05-21 18:38:16 +0300

    changed logic in send* methods

--- src/main/java/ru/bicev/notification_service/service/NotificationService.java
+++ src/main/java/ru/bicev/notification_service/service/NotificationService.java
@@ -1,7 +1,15 @@
 package ru.bicev.notification_service.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import ru.bicev.notification_service.model.Notification;
@@ -10,14 +18,21 @@ import ru.bicev.notification_service.model.Notification;
 public class NotificationService {
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+    private final JavaMailSender mailSender;
+    private String lineSeparator = System.lineSeparator();
+
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+
+    }
 
     public void send(Notification notification) {
         switch (notification.getType()) {
             case EMAIL -> sendEmail(notification);
-            case PUSH -> sendPush(notification);
+            case GENERAL -> sendLog(notification);
             case ALL -> {
                 sendEmail(notification);
-                sendPush(notification);
+                sendLog(notification);
             }
             default -> {
                 logger.warn("Unsupported notification type: {}", notification.getType());
@@ -27,13 +42,29 @@ public class NotificationService {
     }
 
     private void sendEmail(Notification notification) {
-        logger.info("Email notification sent to: {} | subject: {} | message: {}",
-                notification.getEmail(), notification.getSubject(), notification.getMessage());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(notification.getEmail());
+            message.setSubject(notification.getSubject());
+            message.setText(notification.getMessage());
+            mailSender.send(message);
+            logger.info("Email notification sent to: {} | subject: {} | message: {}",
+                    notification.getEmail(), notification.getSubject(), notification.getMessage());
+        } catch (MailException e) {
+            logger.error("Error sending email: {}", notification.getEmail());
+        }
     }
 
-    private void sendPush(Notification notification) {
-        logger.info("Push notification sent to userId: {} | deviceToken: {} | message: {}",
-                notification.getUserId(), notification.getDeviceToken(), notification.getMessage());
+    private void sendLog(Notification notification) {
+        try {
+            Files.writeString(Paths.get("notifications.log"), notification.toString() + lineSeparator,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+            logger.info("Push notification sent to userId: {} | deviceToken: {} | message: {}",
+                    notification.getUserId(), notification.getDeviceToken(), notification.getMessage());
+        } catch (IOException e) {
+            logger.error("Error writing to file: {}", e.getMessage());
+        }
     }
 
 }
